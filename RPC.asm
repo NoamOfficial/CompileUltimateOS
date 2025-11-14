c0a531c46d3aa2427a2efb98c4b24b43381b9e61
NoamOfficial <noamrosenblit@gmail.com>
2025-11-14 15:11:55 +0200

    Update RPC.asm

--- RPC.asm
+++ RPC.asm
@@ -1,55 +1,84 @@
-org 0x130
-resb 65335
-db FF
-org 0x200
-mov es, seg [0x130]
-mov di, 0
-copy:
-lodsb
-cmp al, FF
-je done
-cmp al, 8F
-je ReadMessage
-cmp al, 9F
-je GetService
-cmp al, 02
-je ClearShared
-cmp al, 0F
-je WriteMessage
-ReadMessage:
-push di
-push [es:di+bx]
-pop bx
-jmp copy
-CallService:
-push es
-push di
-mov es, seg [0x900]
-mov di, 0
-search:
-lodsw
-cmp ah, dl
-je CopyService
-add di, 2
-jne search
-CopyService:
-mov cx, ah
-pop es
-pop di
-jmp copy
-ClearShared:
-mov cx, 65335
-mov ds, seg [0x130]
-mov si, 0
-mov al, 0
-loop:
-mov [ds:si], al
-add si, 1
-loop loop
-jmp copy
-WriteMessage:
-mov [0x400], [esp + 2]
-jmp copy
+BITS 32
+
+; -------------------------------
+; Keep original DATA_SELECTOR
+%define DATA_SELECTOR 0x130
+
+section .data
+buffer: resb 65335       ; main buffer
+db 0FFh                  ; sentinel byte at end
+
+section .text
+global _start
+
+_start:
+    ; initialize segment registers
+    mov ax, DATA_SELECTOR
+    mov ds, ax
+    mov es, ax
+
+    ; initialize indexes
+    xor esi, esi          ; source index in DS
+    xor edi, edi          ; destination index in ES
+
+copy_loop:
+    mov al, [ds:esi]
+    cmp al, 0FFh
+    je copy_loop           ; repeat on sentinel
+    cmp al, 8Fh
+    je read_message
+    cmp al, 9Fh
+    je get_service
+    cmp al, 02h
+    je clear_shared
+    cmp al, 0Fh
+    je write_message
+    inc esi
+    jmp copy_loop
+
+; -------------------------------
+read_message:
+    ; copy byte from DS to ES
+    mov al, [ds:esi]
+    mov [es:edi], al
+    inc esi
+    inc edi
+    jmp copy_loop
+
+get_service:
+    ; read service address table at 0x900, put result in AH
+    push esi               ; save current pointer
+    push edi
+    mov esi, 0x900         ; start of Service Address Table
+search_service:
+    lodsw                  ; load word from [ds:esi] into AX, esi += 2
+    cmp ah, dl             ; optional compare, can adjust
+    je copy_service
+    add esi, 2
+    jmp search_service
+copy_service:
+    ; AH now contains the high byte of the service address
+    ; AX already loaded from lodsw
+    pop edi
+    pop esi
+    jmp copy_loop
+
+clear_shared:
+    mov ecx, 65335
+    xor eax, eax
+    xor edi, edi
+clear_loop:
+    mov [es:edi], al
+    inc edi
+    loop clear_loop
+    jmp copy_loop
+
+write_message:
+    ; write value from stack to buffer at offset 0x400
+    mov eax, [esp + 4]      ; 32-bit stack offset
+    mov [es:0x400], eax
+    jmp copy_loop
+
 
 
 
